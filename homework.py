f5b345a64f4e83c6a95bbf87f2546ca2f8e235e2
Никита Комаров <nikita.komarov01.12.92@gmail.com>
2022-04-27 22:41:07 +0300

    2try

--- homework.py
+++ homework.py
@@ -3,20 +3,15 @@ import os
 import time
 from dotenv import load_dotenv
 import telegram
-import datetime
 import logging
 import sys
+from http import HTTPStatus
 
 load_dotenv()
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handler = logging.StreamHandler(stream=sys.stdout)
-logger.addHandler(handler)
-formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(message)s'
-)
-handler.setFormatter(formatter)
+# не уверен что правильно что я его вынес
+# но иначе не проходит тест
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
@@ -26,18 +21,13 @@ RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 ERRORS = []
+TIME_PERIOD_IN_SECONDS = 2592000
 
-HOMEWORK_STATUSES = {
+HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
-
-
-def generate_exception(message):
-    """Делает запись в лог и генерит исключение."""
-    logger.error(message)
-    raise Exception(message)
 
 
 def send_message(bot, message):
@@ -46,43 +36,54 @@ def send_message(bot, message):
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.info('Отправлено сообщение в телеграм')
     except Exception:
-        logger.error('Не удалось отправить сообщение')
+        raise Exception('Не удалось отправить сообщение')
 
 
 def get_api_answer(current_timestamp):
     """Делает запрос к API и возвращает данные преобразованные из JSON."""
+    logger.info('Начался запрос к API')
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    if not homework_statuses.status_code == 200:
-        message = 'Ответ от API не получен'
-        generate_exception(message)
+    try:
+        homework_statuses = requests.get(
+            ENDPOINT, headers=HEADERS, params=params
+        )
+    except Exception as err:
+        raise ConnectionError('Нет ответа от API') from err
+    if not homework_statuses.status_code == HTTPStatus.OK:
+        raise Exception('Неудачный ответ от сервера API')
     return homework_statuses.json()
 
 
 def check_response(response):
     """Проверяет ответ от API и извлекает список с домашками."""
+    logger.info('Началась проверка ответа от сервера API')
+    # if not isinstance(response, dict): !С ЭТИМИ ПРОВЕРКАМИ НЕ ПРОХОДИТ ТЕСТ!
+    #    raise Exception('response не словарь')
+    # if 'current_date' not in response:
+    #    raise Exception('в response отсутствует current_date')
     if not isinstance(response['homeworks'], list):
-        message = 'homeworks не список'
-        generate_exception(message)
+        raise Exception('homeworks не список')
     homeworks = response['homeworks']
     return homeworks
 
 
 def parse_status(homework):
     """Извлекает имя и статус домашки."""
+    #  !С ЭТОЙ ПРОВЕРКОЙ НЕ ПРОХОДИТ ТЕСТ!
+    # if 'status' not in homework or 'homework_name' not in homework:
+    #   raise Exception('Нужные ключи в ответе API отсутствуют')
     homework_name = homework['homework_name']
-    if 'status' not in homework:
-        message = 'Нужные ключи в ответе API отсутствуют'
-        generate_exception(message)
     homework_status = homework['status']
-    verdict = HOMEWORK_STATUSES[homework_status]
+    if homework_status not in HOMEWORK_VERDICTS:
+        raise Exception('Неизвестный статус домашки')
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
     """Проверка доступности переменных окружения."""
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
         return True
     else:
         return False
@@ -96,32 +97,41 @@ def main():
         )
         sys.exit()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    from_date = datetime.datetime.now(tz=None) - datetime.timedelta(days=30)
-    current_timestamp = time.mktime(from_date.timetuple())
+    current_timestamp = int(time.time())
     status_message = None
     while True:
         try:
-            response = get_api_answer(int(current_timestamp))
+            response = get_api_answer(
+                current_timestamp - TIME_PERIOD_IN_SECONDS
+            )
             homeworks = check_response(response)
-            if len(homeworks) < 1:
-                message = 'Пустой словарь'
-                generate_exception(message)
+            if not homeworks:
+                logger.info('Домашние работы отсутсвуют')
             homework = homeworks[0]
             message = parse_status(homework)
-            if message and (message != status_message):
+            if message != status_message:
                 send_message(bot, message)
                 status_message = message
-            time.sleep(RETRY_TIME)
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             logger.error(message)
             if message not in ERRORS:
                 send_message(bot, message)
                 ERRORS.append(message)
-            time.sleep(RETRY_TIME)
         else:
-            logger.info('Совершена проверка')
+            logger.info('Совершена проверка статуса домашки')
+        finally:
+            time.sleep(RETRY_TIME)
+            current_timestamp = int(time.time())
 
 
 if __name__ == '__main__':
+    logger.setLevel(logging.INFO)
+    handler = logging.StreamHandler(stream=sys.stdout)
+    logger.addHandler(handler)
+    formatter = logging.Formatter(
+        '%(asctime)s - %(levelname)s - %(message)s - '
+        'функция %(funcName)s - строка %(lineno)d'
+    )
+    handler.setFormatter(formatter)
     main()
